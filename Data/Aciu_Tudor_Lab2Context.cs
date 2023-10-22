8eb73475e0344d62984fc16a2a7ba6009b88882f
X <X@ZEPHYRUS>
2023-10-22 23:53:56 +0300

    Add project files.

---
+++ Data/Aciu_Tudor_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aciu_Tudor_Lab2.Models;
+
+namespace Aciu_Tudor_Lab2.Data
+{
+    public class Aciu_Tudor_Lab2Context : DbContext
+    {
+        public Aciu_Tudor_Lab2Context (DbContextOptions<Aciu_Tudor_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aciu_Tudor_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Aciu_Tudor_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Aciu_Tudor_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        // Foreign Key
+        public int AuthorID { get; set; }
+        // Navigation Property
+        public Author Author { get; set; }
+    } //navigation property
+
+
+}
